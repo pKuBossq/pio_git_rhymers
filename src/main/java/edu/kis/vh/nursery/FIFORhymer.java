76bd1e335556c0a79bc00d130dddf78612a848a7
Jakub Borkiewicz <borkiewicz.jakub@outlook.com>
2024-05-31 17:24:00 +0200

    17. Szkielet dokumentacji.

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,19 @@
 package edu.kis.vh.nursery;
 
+/**
+ * FIFORhymers class inherits from DefaultCountingOutRhymer class
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Initializing DefaultCountingOutRhymer instance in temp variable
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     *
+     * @return the number of elements removed from the queue. Implementing a queue data structure using two stacks
+     */
     @Override
     public int countOut() {
         while (!callCheck())
@@ -17,6 +27,10 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
         return ret;
     }
 
+    /**
+     *
+     * @return private variable
+     */
     public DefaultCountingOutRhymer getTemp() {
         return temp;
     }
